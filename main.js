e66d90582fb16cb4575fbdd7b18106e787ec81a0
Katie Dawkins <katiemdawkins@gmail.com>
2022-01-12 15:37:31 -0600

    completed exercise

---
+++ main.js
@@ -0,0 +1,13 @@
+const quarterValue = (findQuarter) => {
+    const quarter = findQuarter / 4
+    return quarter
+
+}
+
+const returnedQuarter = quarterValue(24)
+console.log(returnedQuarter)
+
+// Invoke the function and store the return value
+
+
+// Log the return value
